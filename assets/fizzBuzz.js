0fafbb4c88d0ca68404f9f60983f64e9fe5b18c5
ayelenailin-m <pukyailin@gmail.com>
2024-05-13 13:22:20 -0300

    Se terminó el punto 3

--- assets/fizzBuzz.js
+++ assets/fizzBuzz.js
@@ -1,12 +1,11 @@
 
-
-for (let i= 1; i <= 15; i++){
-    if (i%3===0 && i%5===0) {
+for (let i = 1; i <= 15; i++){
+    if (i%3===0 && i%5===0){
         console.log(i+"FIZZBUZZ");
     } else if (i%3===0){
-        console.log(i+"FIZZ")
-    } else if(i%5===0){
-        console.log(i+"BUZZ")
+        console.log(i+"FIZZ");
+    } else if (i%5===0){
+        console.log(i+"BUZZ");
     } else {
         console.log(i);
     }
