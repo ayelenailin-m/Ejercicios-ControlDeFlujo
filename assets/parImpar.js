11b5d953b07bc662da6451019f0d7e41dc6b81f2
ayelenailin-m <pukyailin@gmail.com>
2024-05-13 12:06:25 -0300

    Se terminó el punto 1

---
+++ assets/parImpar.js
@@ -0,0 +1,14 @@
+
+let numero = 0
+
+while (numero <= 10) {
+    if (numero === 0) {
+        console.log(numero);
+    } else if (numero % 2 === 0) {
+        console.log(numero + " es par");
+    
+    } else {
+        console.log(numero + " es impar");
+    }
+    numero++;
+}
